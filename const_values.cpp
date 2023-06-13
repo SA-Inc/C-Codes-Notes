ca2d8ec635f5049c0ffa94e6f063f4ec1499910e
Arman Saparov <s.a.inc.17@gmail.com>
2023-06-13 17:01:24 +0600

    Create const_values.cpp

---
+++ const_values.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+// compile time const - all expressions and assigments while compiling
+// runtime const - expressions and assigments while program running
+
+// if we sure, that value know at compile time - constexpr
+// if we not sure, that value know at compile time, init at runtime - const
+
+// work, but not recomended
+// parameter pass by value (copy), but can not changed
+void print_const(const int p) {
+  // p++;
+  std::cout << p << std::endl;
+}
+
+
+// work, but meaningless (const ignored)
+// because not use const for return
+const int return_const() {
+  return 76;
+}
+
+
+int num_enter() {
+  int num {};
+  std::cin >> num;
+  return num;
+}
+
+
+int main() {
+  // symbolic constants
+  // compile time const
+  const int a {13};
+  print_const(a);
+  std::cout << a << std::endl;
+
+
+  // return 'const' value, but can be changed
+  int b {return_const()};
+  b++;
+  std::cout << b << std::endl;
+
+
+  // runtime const
+  const int y {num_enter()};
+
+
+  // constexpr - only compile time
+  constexpr int e {4 * 6 + 7};
+  std::cout << e << std::endl;
+
+  return 0;
+}
