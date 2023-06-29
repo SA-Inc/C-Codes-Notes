1a9848ab0a0a60c366cff9537f7034b382584313
Arman Saparov <s.a.inc.17@gmail.com>
2023-06-29 20:19:14 +0600

    Update and rename function_overload.cpp to function_overload_default_arguments.cpp

--- function_overload_default_arguments.cpp
+++ function_overload_default_arguments.cpp
@@ -22,10 +22,22 @@ int add(int x, int y, int z) {
 }
 
 
+// default arguments, only copy assigment, not list assigment
+// default arguments must be init from right side, on left side passed arguments
+// if use forward declaration, can not reassigment
+void default_arguments(int a = 10, int b = 20) {
+  std::cout << a << " " << b << std::endl;
+}
+
+
 int main() {
   std::cout << add(1, 1) << std::endl;
   std::cout << add(1.2, 1.6) << std::endl;
   std::cout << add(1, 2, 1) << std::endl;
+
+  default_arguments(1, 2);
+  default_arguments(1);
+  default_arguments();
 
   return 0;
 }
